9bd83c5cad980491dac1532fb6d1610bef026cec
mica1105 <44710351+mica1105@users.noreply.github.com>
2021-08-28 15:58:04 -0300

    Initial commit
    inicio

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Coca cola es la bebida mas tomada en el mundo">
+    <meta name="keywords" content="bebida, gaseosa, cola">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio - CocaCola</title>
+    <link rel="shortcut icon" href="img/favicon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/cfd3e67979.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <a href="index.html">
+                <img src="img/logo.svg" class="logo" alt="logo">
+            </a>
+            <nav>
+                <a href="#inicio">Inicio</a>
+                <a href="#nosotros">Nosotros</a>
+                <a href="#servicios">Servicios</a>
+                <a href="#galeria">Galeria</a>
+                <a href="#contactenos">Contactenos</a>
+            </nav>
+            <a href="#" class="hamb"><i class="fas fa-bars"></i></a>
+        </div>
+    </header>
+    <main>
+        <section id="inicio">
+            <img src="img/bannerprincipal.jpg" alt="banner">
+            <div class="bloque-inicio">
+                <h1>Bienvenidos a CocaCola</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <a href="#nosotros" class="boton boton-rojo">Ver más</a>
+            </div>
+        </section>
+
+        <section id="nosotros" class="seccion">
+            <div class="container">
+                <p>
+                    Vestibulum tincidunt iaculis arcu non efficitur. Nunc bibendum elit eu est tempor fermentum. Donec ornare augue a sollicitudin lacinia. Morbi euismod tincidunt laoreet. Duis congue sem erat. Phasellus nibh risus, hendrerit sed ante eu, mollis imperdiet nisl. Duis ac cursus nunc, in egestas mi. Sed ac suscipit massa, vitae rutrum magna. Suspendisse condimentum, nunc eget imperdiet sollicitudin, tellus nibh pulvinar nibh, et blandit elit nunc vel nisi. Sed laoreet condimentum imperdiet.
+                </p>
+            </div>
+        </section>
+
+        <section id="servicios" class="seccion">
+            <div class="container">
+                <div class="row">
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio1.jpg" alt="servicio1">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servivio 1</h3>
+                                <p>
+                                    Sed ac suscipit massa, vitae rutrum magna. Suspendisse condimentum, nunc eget imperdiet sollicitudin, tellus nibh pulvinar nibh, et blandit elit nunc vel nisi.
+                                </p>
+                                <a href="#" class="boton boton-blanco">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio2.jpg" alt="servicio2">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servivio 2</h3>
+                                <p>
+                                    Sed ac suscipit massa, vitae rutrum magna. Suspendisse condimentum, nunc eget imperdiet sollicitudin, tellus nibh pulvinar nibh, et blandit elit nunc vel nisi.
+                                </p>
+                                <a href="#" class="boton boton-blanco">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio3.jpg" alt="servicio3">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servivio 3</h3>
+                                <p>
+                                    Sed ac suscipit massa, vitae rutrum magna. Suspendisse condimentum, nunc eget imperdiet sollicitudin, tellus nibh pulvinar nibh, et blandit elit nunc vel nisi.
+                                </p>
+                                <a href="#" class="boton boton-blanco">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="galeria">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg">
+                            <h4>Imagen 1</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio2.jpg">
+                            <h4>Imagen 2</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio3.jpg">
+                            <h4>Imagen 3</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg">
+                            <h4>Imagen 4</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio2.jpg">
+                            <h4>Imagen 5</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio3.jpg">
+                            <h4>Imagen 6</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="contactenos" class="seccion">
+            <iframe width="520" height="400" frameborder="0" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20San%20Luis+(San%20Luis-%20Argentina)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+                        <form action="index.php" method="POST">
+                            <div class="form-block">
+                                <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+                            </div>
+                            <div class="form-block">
+                                <input type="email" name="email" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-block">
+                                <textarea name="mensaje" placeholder="Mensaje"></textarea>
+                            </div>
+                            <div class="form-block bloque-ultimo">
+                                <input type="submit" class="boton boton-negro" value="Enviar">
+                            </div>
+                            <?php
+                            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                $nombre = $_POST["nombre"];
+                                $email = $_POST["email"];
+                                $mensaje = $_POST["mensaje"];
+
+                                if (isset($nombre)) {
+                                    if (isset($email)) {
+                                        if (isset($mensaje)) {
+                                            $para = "mika.dure1105@gmail.com";
+                                            $asunto = " Esto es una prueba";
+                                            $cuerpo = $nombre . "\n" . $email . "\n" . $mensaje;
+                                            $adicional = 'From: noreply@mikadure.com';
+
+                                            if (mail($para, $asunto, $cuerpo, $adicional)) {
+                                                echo "<p> Enviado Exitosamente </p>";
+                                            } else {
+                                                echo "<p> Error al enviar </p>";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            ?>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="seccion">
+        <div class="container">
+            <div class="row">
+                <div class="columna columna-25 columna-mobile-100">
+                    <img src="img/logo-blanco.png" class="logo-footer">
+                    <p>
+                        Sed ac suscipit massa, vitae rutrum magna. Suspendisse condimentum, nunc eget imperdiet sollicitudin, tellus nibh pulvinar.
+                    </p>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>Temas Relacionados</h3>
+                    <ul>
+                        <li><a href="#">Tema 1</a></li>
+                        <li><a href="#">Tema 2</a></li>
+                        <li><a href="#">Tema 3</a></li>
+                        <li><a href="#">Tema 4</a></li>
+                    </ul>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>Datos de Contactos</h3>
+                    <ul>
+                        <li>mika.dure1105@gmail.com</li>
+                        <li>+542664540633</li>
+                        <li>Brio Eusebio Castaño Mz.A C.9 <br> San Luis - Argentina <br> </li>
+                    </ul>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>Redes Sociales</h3>
+                    <ul class="redes">
+                        <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram-square"></i></a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="barra-footer">
+            &copy; Derechos Reservados - 2021
+        </div>
+    </footer>
+    <script src="js/jquery.js"></script>
+    <script src="js/funciones.js"></script>
+</body>
+
+</html>
